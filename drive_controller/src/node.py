c2ea4604e094386b6fca9ba5037a52ba4646955a
younes <yreda@uwaterloo.ca>
2021-08-30 16:32:27 -0400

    added motorkit_robot code

--- drive_controller/src/node.py
+++ drive_controller/src/node.py
@@ -1,60 +1,21 @@
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
-from adafruit_motorkit import MotorKit
-
-
-class FeatherWing:
-    def __init__(self, max_speed):
-        self.__drivetrain = MotorKit()
-        self.__left = self.__drivetrain.motor1
-        self.__right = self.__drivetrain.motor2
-        self.__max_speed = max_speed
-
-    # Speed [-max_speed, +max_speed]
-    def SetLeftMotorSpeed(self, speed):
-        speed = speed / self.__max_speed
-        speed = min(1, max(-1, speed))
-        self.__left.throttle(speed)
-
-    # Speed [-max_speed, +max_speed]
-    def SetRightMotorSpeed(self, speed):
-        speed = speed / self.__max_speed
-        speed = min(1, max(-1, speed))
-        self.__right.throttle(speed)
-
-
-class Controller:
-    def __init__(self, topic, wheel_distance, wheel_radius, max_speed):
-        self.topic = topic
-        self.wheel_distance = wheel_distance
-        self.wheel_radius = wheel_radius
-        self.fw = FeatherWing(max_speed)
-
-    def twist_to_pair(self, twist):
-        left_speed = (twist.linear.x - twist.angular.z *
-                      self.wheel_distance/2)/self.wheel_radius
-        right_speed = (twist.linear.x + twist.angular.z *
-                       self.wheel_distance/2) / self.wheel_radius
-        return (left_speed, right_speed)
-
-    def callback(self, data):
-        [left, right] = self.twist_to_pair(data)
-        self.fw.SetLeftMotorSpeed(left)
-        self.fw.SetRightMotorSpeed(right)
-        rospy.loginfo("Setting motor speed")
+from motorkit_robot import Robot
 
 
 def main():
     rospy.init_node('drive_controller')
 
     topic = rospy.get_param("/drive_controller/topic")
-    wheel_speed = rospy.get_param("/drive_controller/max_wheel_speed")
-    wheel_radius = rospy.get_param("/drive_controller/wheel_radius")
-    wheel_seperation = rospy.get_param("/drive_controller/wheel_seperation")
-    controller = Controller(topic, wheel_seperation, wheel_radius, wheel_speed)
+    left_trim = rospy.get_param("/drive_controller/left_trim")
+    right_trim = rospy.get_param("/drive_controller/right_trim")
 
-    rospy.Subscriber(topic, Twist, controller.callback)
+    robot = Robot(left_trim=left_trim, right_trim=right_trim)
+
+    # younes todo the steer function cant handle negative speeds, might behave weirdly
+    rospy.Subscriber(topic, Twist, lambda twist: robot.steer(twist.linear.x, -twist.angular.z)
+                     )
     rospy.spin()
 
 
--- drive_controller/src/run.py
+++ drive_controller/src/run.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
+import motorkit_robot
+import rospy
 import time
-from adafruit_motorkit import MotorKit
-# younes todo create proper fw library and use it in both node and here
-# younes todo this does not terminate well use ros::ok
-# younes todo the node.py uses .throttle(speed) instead of .throttle = speed, fix that
-# also the fucking motor driver beeps at no speed (torque aint strong enough)
-def main():
-    kit = MotorKit()
-    while True:
-        kit.motor1.throttle = 0.5
-        time.sleep(0.5)
-        kit.motor1.throttle = 0
-        time.sleep(0.5)
 
-        
+
+def main():
+    LEFT_TRIM = 0
+    RIGHT_TRIM = 0
+
+    robot = motorkit_robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
+    while not rospy.is_shudown():
+        for i in range(0, 1, 0.1):
+            robot.forward(i, 0.25)
+        for i in range(0, 1, 0.1):
+            robot.backward(i, 0.25)
+        robot.stop()
+        time.sleep(1)
+
+
 if __name__ == '__main__':
     main()
-
