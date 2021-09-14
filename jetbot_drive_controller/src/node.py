3743fb1ff814e74be817d9b3b41030c96fb33c09
younes <yreda@uwaterloo.ca>
2021-09-14 19:10:48 -0400

    prefixed all packages with jetbot

--- jetbot_drive_controller/src/node.py
+++ jetbot_drive_controller/src/node.py
@@ -5,11 +5,11 @@ from motorkit_robot import Robot
 
 
 def main():
-    rospy.init_node('drive_controller')
+    rospy.init_node('jetbot_drive_controller')
 
-    topic = rospy.get_param("/drive_controller/topic")
-    left_trim = rospy.get_param("/drive_controller/left_trim")
-    right_trim = rospy.get_param("/drive_controller/right_trim")
+    topic = rospy.get_param("/jetbot_drive_controller/topic")
+    left_trim = rospy.get_param("/jetbot_drive_controller/left_trim")
+    right_trim = rospy.get_param("/jetbot_drive_controller/right_trim")
 
     robot = Robot(left_trim=left_trim, right_trim=right_trim)
     func = lambda twist : robot.linear(twist.linear.x) if twist.linear.x != 0 else robot.angular(twist.angular.z) 
