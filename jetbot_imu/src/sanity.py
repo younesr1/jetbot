bbdd50af40a511c260b276aff4a7ebf1ecb7c5f6
younesr1 <yreda@uwaterloo.ca>
2021-09-16 17:55:43 -0400

    tested sanity.py

--- jetbot_imu/src/sanity.py
+++ jetbot_imu/src/sanity.py
@@ -6,10 +6,11 @@
 import time
 import adafruit_mpu6050
 # import rospy
-
+import board
 
 def main():
-    mpu = adafruit_mpu6050.MPU6050(0, 0x68)
+    i2c = board.I2C()  # uses board.SCL and board.SDA
+    mpu = adafruit_mpu6050.MPU6050(i2c) # connects to bus 1
 
     while True:  # not rospy.is_shutdown():
         print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" %
@@ -17,7 +18,7 @@ def main():
         print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
         print("Temperature: %.2f C" % mpu.temperature)
         print("")
-        time.sleep(0.5)
+        time.sleep(1/10.0)
 
 
 if __name__ == '__main__':
